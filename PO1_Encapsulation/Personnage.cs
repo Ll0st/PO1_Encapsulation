d346171e837117b4c6329ad35610ecddc97ae6b6
Brandi <98344837+Ll0st@users.noreply.github.com>
2024-02-08 14:30:19 -0500

    Create Personnage.cs

---
+++ PO1_Encapsulation/Personnage.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PO1_Encapsulation
+{
+    internal class Personnage
+    {
+        private string nom;
+        private int pointDeVie;
+        private int pointsDePuissance;
+
+        public string Nom { get => nom; set => nom = value; }
+        public int PointDeVie { 
+            get => pointDeVie;
+            set
+            {
+                if (value > 0)
+                {
+                    pointDeVie = value;
+                }
+                else
+                {
+                    pointDeVie = 0;
+                }
+            }
+        }
+        public int PointsDePuissance { get => pointsDePuissance; set => pointsDePuissance = value; }
+
+        public bool EstEnVie
+        {
+            get
+            {
+                if(pointDeVie > 0)
+                {
+                    return true;
+                }
+                return false;
+            }
+        }
+    }
+}
